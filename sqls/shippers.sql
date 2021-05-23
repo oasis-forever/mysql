24cd8327339152241c64f7e8dbe17cd221d145d4
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-05-23 22:29:55 +0900

    Commit SQL files

---
+++ sqls/shippers.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Shippers(
+  ShipperID int NOT NULL,
+  ShipperName varchar(255),
+  Phone varchar(255),
+  PRIMARY KEY (ShipperID)
+);
+
+INSERT INTO Shippers VALUES(1, "Speedy Express", "(503) 555-9831");
+INSERT INTO Shippers VALUES(2, "United Package", "(503) 555-3199");
+INSERT INTO Shippers VALUES(3, "Federal Shipping", "(503) 555-9931");
+
+SELECT * FROM Shippers;
